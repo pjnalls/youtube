36cc0994193be5104e5c25fc313266efa5d20796
pjnalls <42269808+pjnalls@users.noreply.github.com>
2024-08-16 13:56:33 -0500

    🆕 feat: animated switch; customized tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Link, Tabs } from 'expo-router';
 import { Pressable } from 'react-native';
 
@@ -7,17 +8,34 @@ import Colors from '@/constants/Colors';
 import { useColorScheme } from 'nativewind';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 import { View } from '@/components/Themed';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof MaterialCommunityIcons>['name'];
   color: string;
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+  return (
+    <MaterialCommunityIcons size={28} style={{ marginBottom: -3 }} {...props} />
+  );
 }
 
 export default function TabLayout() {
   const { colorScheme, toggleColorScheme } = useColorScheme();
+  const switchOpacity = useSharedValue(0);
+  const switchFadeIn = () => {
+    switchOpacity.value = withTiming(1, { duration: 300 });
+  };
+
+  useEffect(() => switchFadeIn(), []);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: switchOpacity.value,
+  }));
 
   return (
     <Tabs
@@ -31,8 +49,13 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) =>
+            focused ? (
+              <TabBarIcon name="home" color={color} />
+            ) : (
+              <TabBarIcon name="home-outline" color={color} />
+            ),
           headerRight: () => (
             <View style={{ flexDirection: 'row' }}>
               <Link href="/modal" asChild>
@@ -47,14 +70,23 @@ export default function TabLayout() {
                   )}
                 </Pressable>
               </Link>
-              <Pressable onPress={toggleColorScheme} style={{ width: 32 }}>
+              <Pressable
+                onPress={() => {
+                  toggleColorScheme();
+                  switchOpacity.value = 0;
+                  switchFadeIn();
+                }}
+                style={{ width: 32 }}
+              >
                 {({ pressed }) => (
-                  <FontAwesome
-                    name={colorScheme === 'dark' ? 'sun-o' : 'moon-o'}
-                    size={24}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
+                  <Animated.View style={animatedStyle}>
+                    <FontAwesome
+                      name={colorScheme === 'dark' ? 'sun-o' : 'moon-o'}
+                      size={24}
+                      color={Colors[colorScheme ?? 'light'].text}
+                      style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                    />
+                  </Animated.View>
                 )}
               </Pressable>
             </View>
@@ -62,10 +94,17 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="access"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Access',
+          tabBarIcon: ({ color, focused }) => focused ? <TabBarIcon name="key" color={color} /> : <TabBarIcon name="key-outline" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="help"
+        options={{
+          title: 'Help',
+          tabBarIcon: ({ color, focused }) => focused ? <TabBarIcon name="information" color={color} /> : <TabBarIcon name="information-outline" color={color} />,
         }}
       />
     </Tabs>
