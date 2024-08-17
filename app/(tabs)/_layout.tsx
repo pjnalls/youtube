2742557c6c56f13c287e14a5af49ecae66b0711e
pjnalls <42269808+pjnalls@users.noreply.github.com>
2024-08-17 06:37:32 -0500

    💅 style: run `npx prettier . --write`

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,104 +9,139 @@ import { useColorScheme } from 'nativewind';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 import { View } from '@/components/Themed';
 import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming,
+	useAnimatedStyle,
+	useSharedValue,
+	withTiming,
 } from 'react-native-reanimated';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof MaterialCommunityIcons>['name'];
-  color: string;
+	name: React.ComponentProps<typeof MaterialCommunityIcons>['name'];
+	color: string;
 }) {
-  return (
-    <MaterialCommunityIcons size={28} style={{ marginBottom: -3 }} {...props} />
-  );
+	return (
+		<MaterialCommunityIcons
+			size={28}
+			style={{ marginBottom: -3 }}
+			{...props}
+		/>
+	);
 }
 
 export default function TabLayout() {
-  const { colorScheme, toggleColorScheme } = useColorScheme();
-  const switchOpacity = useSharedValue(0);
-  const switchFadeIn = () => {
-    switchOpacity.value = withTiming(1, { duration: 300 });
-  };
+	const { colorScheme, toggleColorScheme } = useColorScheme();
+	const switchOpacity = useSharedValue(0);
+	const switchFadeIn = () => {
+		switchOpacity.value = withTiming(1, { duration: 300 });
+	};
 
-  useEffect(() => switchFadeIn(), []);
+	useEffect(() => switchFadeIn(), []);
 
-  const animatedStyle = useAnimatedStyle(() => ({
-    opacity: switchOpacity.value,
-  }));
+	const animatedStyle = useAnimatedStyle(() => ({
+		opacity: switchOpacity.value,
+	}));
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) =>
-            focused ? (
-              <TabBarIcon name="home" color={color} />
-            ) : (
-              <TabBarIcon name="home-outline" color={color} />
-            ),
-          headerRight: () => (
-            <View style={{ flexDirection: 'row' }}>
-              <Link href="/modal" asChild>
-                <Pressable>
-                  {({ pressed }) => (
-                    <FontAwesome
-                      name="info-circle"
-                      size={25}
-                      color={Colors[colorScheme ?? 'light'].text}
-                      style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                    />
-                  )}
-                </Pressable>
-              </Link>
-              <Pressable
-                onPress={() => {
-                  toggleColorScheme();
-                  switchOpacity.value = 0;
-                  switchFadeIn();
-                }}
-                style={{ width: 32 }}
-              >
-                {({ pressed }) => (
-                  <Animated.View style={animatedStyle}>
-                    <FontAwesome
-                      name={colorScheme === 'dark' ? 'sun-o' : 'moon-o'}
-                      size={24}
-                      color={Colors[colorScheme ?? 'light'].text}
-                      style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                    />
-                  </Animated.View>
-                )}
-              </Pressable>
-            </View>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="access"
-        options={{
-          title: 'Access',
-          tabBarIcon: ({ color, focused }) => focused ? <TabBarIcon name="key" color={color} /> : <TabBarIcon name="key-outline" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="help"
-        options={{
-          title: 'Help',
-          tabBarIcon: ({ color, focused }) => focused ? <TabBarIcon name="information" color={color} /> : <TabBarIcon name="information-outline" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+				// Disable the static render of the header on web
+				// to prevent a hydration error in React Navigation v6.
+				headerShown: useClientOnlyValue(false, true),
+			}}
+		>
+			<Tabs.Screen
+				name='index'
+				options={{
+					title: 'Home',
+					tabBarIcon: ({ color, focused }) =>
+						focused ? (
+							<TabBarIcon
+								name='home'
+								color={color}
+							/>
+						) : (
+							<TabBarIcon
+								name='home-outline'
+								color={color}
+							/>
+						),
+					headerRight: () => (
+						<View style={{ flexDirection: 'row' }}>
+							<Link
+								href='/modal'
+								asChild
+							>
+								<Pressable>
+									{({ pressed }) => (
+										<FontAwesome
+											name='info-circle'
+											size={25}
+											color={Colors[colorScheme ?? 'light'].text}
+											style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+										/>
+									)}
+								</Pressable>
+							</Link>
+							<Pressable
+								onPress={() => {
+									toggleColorScheme();
+									switchOpacity.value = 0;
+									switchFadeIn();
+								}}
+								style={{ width: 32 }}
+							>
+								{({ pressed }) => (
+									<Animated.View style={animatedStyle}>
+										<FontAwesome
+											name={colorScheme === 'dark' ? 'sun-o' : 'moon-o'}
+											size={24}
+											color={Colors[colorScheme ?? 'light'].text}
+											style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+										/>
+									</Animated.View>
+								)}
+							</Pressable>
+						</View>
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name='access'
+				options={{
+					title: 'Access',
+					tabBarIcon: ({ color, focused }) =>
+						focused ? (
+							<TabBarIcon
+								name='key'
+								color={color}
+							/>
+						) : (
+							<TabBarIcon
+								name='key-outline'
+								color={color}
+							/>
+						),
+				}}
+			/>
+			<Tabs.Screen
+				name='help'
+				options={{
+					title: 'Help',
+					tabBarIcon: ({ color, focused }) =>
+						focused ? (
+							<TabBarIcon
+								name='information'
+								color={color}
+							/>
+						) : (
+							<TabBarIcon
+								name='information-outline'
+								color={color}
+							/>
+						),
+				}}
+			/>
+		</Tabs>
+	);
 }
--- app/(tabs)/help.tsx
+++ app/(tabs)/help.tsx
@@ -1,31 +1,48 @@
 import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { ExternalLink } from '@/components/ExternalLink';
 
 export default function HelpScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Help</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/help.tsx" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Getting access to this app</Text>
+			<View
+				style={styles.separator}
+				lightColor='#eee'
+				darkColor='rgba(255,255,255,0.1)'
+			/>
+			<Text>
+				To get access to this app you'll need an API key for the YouTube Data
+				API. Instructions are available{' '}
+				<ExternalLink href='https://developers.google.com/youtube/v3/getting-started'>
+					here
+				</ExternalLink>{' '}
+				for how to get one. It's highly recommended that you restrict your API
+				key which you can find out how to do{' '}
+				<ExternalLink href='https://developers.google.com/youtube/registering_an_application#:~:text=If%20the%20API%20key%20that,selecting%20one%20of%20the%20Restrictions.'>
+					here
+				</ExternalLink>
+				.
+			</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'flex-start',
+		justifyContent: 'center',
+		padding: 16,
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		textAlign: 'left',
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: '80%',
+	},
 });
